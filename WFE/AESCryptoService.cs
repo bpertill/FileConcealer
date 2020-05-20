ef8bc7b85a08502544769dfd8543bf8faff73215
Bernhard Pertiller <bernhard.pertiller91@gmail.com>
2020-05-20 19:25:45 +0200

    Add project files.

---
+++ WFE/AESCryptoService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+
+namespace WFE
+{
+    public class AESCryptoService
+    {
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        public void FileEncrypt(string inputFile, string password)
+        {
+            byte[] salt = GenerateRandomSalt();
+            
+            using FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Mode = CipherMode.CBC;
+
+            // write salt to the begining of the output file
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            using CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            using FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //1MB buffer
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Message);
+            }
+        }
+
+        public void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            using FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+
+            using CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            using FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+   
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ WFE/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Security.Permissions;
+using System.Diagnostics;
+
+namespace WFE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> _encPaths;
+        public ObservableCollection<string> EncPaths
+        {
+            get { return _encPaths; }
+            set { _encPaths = value; }
+        }
+        AESCryptoService _aes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _aes = new AESCryptoService();
+            EncPaths = new ObservableCollection<string>();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            EncPaths.Clear();
+        }
+
+        private void Enctypt_Click(object sender, RoutedEventArgs e)
+        {
+            EncDecrypt(Mode.Encrypt);
+        }
+
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            EncDecrypt(Mode.Decrypt);
+        }
+
+        private async void EncDecrypt(Mode mode)
+        {
+           
+            ProgressB.Maximum = EncPaths.Count;
+            ProgressB.Value = 0;
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(EncPaths, (filename) =>
+                {
+                    if (mode == Mode.Encrypt)
+                        _aes.FileEncrypt(filename, PwBox.Password);
+
+                    else if (mode == Mode.Decrypt)
+                        _aes.FileDecrypt(filename, $"{filename.Substring(0, filename.Length - ".aes".Length)}", PwBox.Password);
+                    Dispatcher.Invoke(() =>
+                    {
+                        ProgressB.Value++;
+                    });
+                });
+                 });
+            EncPaths.Clear();
+        }
+        public enum Mode
+        {
+            Encrypt,
+            Decrypt
+        }
+
+        private void LoadFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog { Multiselect = true };
+            if (openFileDialog.ShowDialog() == true)
+                foreach (var item in openFileDialog.FileNames)
+                {
+                    EncPaths.Add(item);
+                }
+        }
+    }
+}
